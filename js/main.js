16e8a43dff791aa7c4faf8d11c5bbd4c1fe6cf05
Julia Saviuk <julia.saviyk@gmail.com>
2019-08-18 18:11:07 +0200

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -20,34 +20,15 @@ window.onload = function() {
   }
   //Actions executed on click
   $(".openCell").click(function() {
-    // Removing background color to the current raw
-
-    $(newGame.currentCell)
-      .parent()
-      .removeClass("bg");
-    // Removing background color to the current column
-
-    if (newGame.currentCell) {
-      for (let i = 0; i < newGame.game.length; i++) {
-        for (let j = 0; j < newGame.game[i].length; j++) {
-          if (j === newGame.currentCell.cellIndex) {
-            $(
-              "table tr:nth-child(" +
-                (i + 1) +
-                ") td:nth-child(" +
-                (j + 1) +
-                ")"
-            ).removeClass("bg");
-          }
-        }
-      }
-    }
+    // Removing background color
+    $("tr, td").removeClass("bg");
 
     newGame.currentCell = this;
     // Adding background color to the current raw
     $(newGame.currentCell)
       .parent()
       .addClass("bg");
+
     // Adding background color to the current column
     for (let i = 0; i < newGame.game.length; i++) {
       for (let j = 0; j < newGame.game[i].length; j++) {
@@ -63,12 +44,16 @@ window.onload = function() {
   window.addEventListener("keyup", function(event) {
     if (49 <= event.keyCode && event.keyCode <= 57) {
       const { currentCell, game } = newGame;
+      $(currentCell).click();
+
       currentCell.innerText = event.key;
 
       var cellIndex = $(currentCell).index();
       var rowIndex = $(currentCell)
         .parent()
         .index();
+
+      $("td").removeClass("correct");
 
       if (game[rowIndex][cellIndex] == event.key) {
         $(currentCell).addClass("correct");
