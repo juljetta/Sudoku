8dc820fc93ed89df5069f402320e84101d1ec1bd
Julia Saviuk <julia.saviyk@gmail.com>
2019-08-18 23:02:47 +0200

    Validation done

--- js/main.js
+++ js/main.js
@@ -3,11 +3,18 @@ window.onload = function() {
 
   $("td").addClass("openCell");
 
+  function getHTMLCell(i, j) {
+    var cell = $(
+      "table tr:nth-child(" + (i + 1) + ") td:nth-child(" + (j + 1) + ")"
+    );
+    return cell;
+  }
+
   //  Record numbers of the game in cells and hide random 30 numbers
   for (let i = 0; i < newGame.game.length; i++) {
     for (let j = 0; j < newGame.game[i].length; j++) {
       if (newGame.hiddenNumbers.indexOf(i + ", " + j) === -1) {
-        $("table tr:nth-child(" + (i + 1) + ") td:nth-child(" + (j + 1) + ")")
+        getHTMLCell(i, j)
           .text(newGame.game[i][j])
           .removeClass("openCell");
       }
@@ -17,7 +24,10 @@ window.onload = function() {
   $(".openCell").click(function() {
     // Removing background color
     $("tr, td").removeClass("bg");
+    $("td").removeClass("current-cell");
+
     newGame.currentCell = this;
+    $(newGame.currentCell).addClass("current-cell");
 
     // Adding background color to the current raw
     $(newGame.currentCell)
@@ -28,9 +38,7 @@ window.onload = function() {
     for (let i = 0; i < newGame.game.length; i++) {
       for (let j = 0; j < newGame.game[i].length; j++) {
         if (j === newGame.currentCell.cellIndex) {
-          $(
-            "table tr:nth-child(" + (i + 1) + ") td:nth-child(" + (j + 1) + ")"
-          ).addClass("bg");
+          getHTMLCell(i, j).addClass("bg");
         }
       }
     }
@@ -50,9 +58,7 @@ window.onload = function() {
     // debugger;
     for (let i = startI; i <= startI + 2; i++) {
       for (let j = startJ; j <= startJ + 2; j++) {
-        $(
-          "table tr:nth-child(" + (i + 1) + ") td:nth-child(" + (j + 1) + ")"
-        ).addClass("bg");
+        getHTMLCell(i, j).addClass("bg");
       }
     }
   });
@@ -64,33 +70,84 @@ window.onload = function() {
 
       currentCell.innerText = event.key;
 
-      var cellIndex = $(currentCell).index();
-      var rowIndex = $(currentCell)
-        .parent()
-        .index();
-
-      $("td").removeClass("correct");
-
-      if (game[rowIndex][cellIndex] == event.key) {
-        $(currentCell).addClass("correct");
-      }
+      // Validation with the final result
+      // var cellIndex = $(currentCell).index();
+      // var rowIndex = $(currentCell)
+      //   .parent()
+      //   .index();
+      // $("td").removeClass("correct");
+      // if (game[rowIndex][cellIndex] == event.key) {
+      //   $(currentCell).addClass("correct");
+      // }
+      checkConflicts();
     } else {
       return false;
     }
 
     for (let i = 0; i < newGame.game.length; i++) {
       for (let j = 0; j < newGame.game[i].length; j++) {
-        if (
-          $(newGame.currentCell).text() ===
-          $(
-            "table tr:nth-child(" + (i + 1) + ") td:nth-child(" + (j + 1) + ")"
-          ).text()
-        ) {
-          $(
-            "table tr:nth-child(" + (i + 1) + ") td:nth-child(" + (j + 1) + ")"
-          ).addClass("bg");
+        if ($(newGame.currentCell).text() === getHTMLCell(i, j).text()) {
+          getHTMLCell(i, j).addClass("bg");
         }
       }
     }
   });
+  function checkConflicts() {
+    $("td").removeClass("conflict-cell");
+    $("td").removeClass("conflict-open-cell");
+
+    $(".openCell").each(function(_, cell) {
+      if ($(cell).text() != "") {
+        var inputValue = $(cell).text();
+        var cellIndex = $(cell).index();
+        var rowIndex = $(cell)
+          .parent()
+          .index();
+
+        // Validating row
+        for (let i = 0; i < 9; i++) {
+          var currentHTMLCell = getHTMLCell(rowIndex, i);
+          if (currentHTMLCell.text() == inputValue) {
+            if (currentHTMLCell.hasClass("openCell")) {
+              currentHTMLCell.addClass("conflict-open-cell");
+            } else {
+              currentHTMLCell.addClass("conflict-cell");
+            }
+          }
+        }
+
+        // Validating column
+        for (let i = 0; i < 9; i++) {
+          var currentHTMLCell = getHTMLCell(i, cellIndex);
+          if (currentHTMLCell.text() == inputValue) {
+            if (currentHTMLCell.hasClass("openCell")) {
+              currentHTMLCell.addClass("conflict-open-cell");
+            } else {
+              currentHTMLCell.addClass("conflict-cell");
+            }
+          }
+        }
+
+        //Validating Square
+        var startingCoordinates = newGame.findStartingCoordinates(
+          rowIndex,
+          cellIndex
+        );
+        var startI = startingCoordinates[0];
+        var startJ = startingCoordinates[1];
+        for (let i = startI; i <= startI + 2; i++) {
+          for (let j = startJ; j <= startJ + 2; j++) {
+            var currentHTMLCell = getHTMLCell(i, j);
+            if (currentHTMLCell.text() == inputValue) {
+              if (currentHTMLCell.hasClass("openCell")) {
+                currentHTMLCell.addClass("conflict-open-cell");
+              } else {
+                currentHTMLCell.addClass("conflict-cell");
+              }
+            }
+          }
+        }
+      }
+    });
+  }
 };
